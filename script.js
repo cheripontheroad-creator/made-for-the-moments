f56cd7d19a20a5806781ce5a794702ebca918504
cheripontheroad-creator <cheripontheroad@gmail.com>
2026-07-13 20:48:38 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+if ("scrollRestoration" in history) {
+  history.scrollRestoration = "manual";
+}
+
+window.addEventListener("pageshow", function () {
+  window.scrollTo(0, 0);
+});
+
 document.addEventListener("DOMContentLoaded", function () {
   const year = document.getElementById("year");
   if (year) {
